9076dd4882d401dd3bb9565df2970c491fd4ae4b
FormDigitalMedia <73562718+FormDigitalMedia@users.noreply.github.com>
2021-11-12 13:21:20 +1000

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,18 @@
+export default {
+  ssr: false,
+  target: "static",
+  // Auto-load components/
+  components: true,
+  /*
+   ** Nuxt.js modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    "@nuxtjs/tailwindcss"
+  ],
+  tailwindcss: {
+    jit: true,
+    // add '~tailwind.config` alias
+    exposeConfig: true
+  }
+};
